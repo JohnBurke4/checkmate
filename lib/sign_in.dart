3902a7fe720bdd33caa443cca6b134a8d9ebb1de
Luiz Fellipe Montibeller <60435968+lzfellipe@users.noreply.github.com>
2022-02-11 17:42:28 +0000

    changes

---
+++ lib/sign_in.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class SignInScreen extends StatefulWidget {
+  const SignInScreen({ Key? key }) : super(key: key);
+
+  @override
+  _SignInScreenState createState() => _SignInScreenState();
+}
+
+class _SignInScreenState extends State<SignInScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(colors: [
+          Colors.white,
+          Colors.blue
+        ],begin: Alignment.topCenter,end: Alignment.bottomCenter)),
+      )
+    );
+  }
+}
