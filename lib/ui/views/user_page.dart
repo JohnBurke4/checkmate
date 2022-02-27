63c1b1569c8d389b1b71a417d025a5ba5b8bef2c
Oisin Tong <tongo@tcd.ie>
2022-02-27 11:58:44 +0000

    changes in user page and edit page are communicated to one another

--- lib/ui/views/user_page.dart
+++ lib/ui/views/user_page.dart
@@ -1,13 +1,27 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter/cupertino.dart';
 import 'edit_user_page.dart';
-import ''
+import 'package:checkmate/models/user.dart';
 class UserPage extends StatelessWidget {
+  User user= User("Magnus C", "My bio");
+
   @override
   Widget build(BuildContext buildContext){
-    return new Scaffold(
-    body: new Column(
+    log("build");
+    if(ModalRoute.of(buildContext)!.settings.arguments != null) {
+      User temp = ModalRoute.of(buildContext)!.settings.arguments as User;
+      user.name = temp.name;
+      user.bio = temp.bio;
+      log(temp.name);
+      log(temp.bio);
+    }
+
+    return  Scaffold(
+    resizeToAvoidBottomInset: false,
+    body:  Column(
       children: <Widget>[
         SizedBox(height: 30, ),
         Stack(
@@ -17,11 +31,10 @@ class UserPage extends StatelessWidget {
             ]
         ),
         SizedBox(height: 20, ),
-        ListTile(title: Text('Magnus Carlsen', textAlign: TextAlign.center, style: TextStyle( fontSize: 35, fontWeight: FontWeight.bold),), subtitle: Text('The King in The North', textAlign: TextAlign.center, style: TextStyle( fontSize: 20),))
+        ListTile(title: Text(user.name, textAlign: TextAlign.center, style: TextStyle( fontSize: 35, fontWeight: FontWeight.bold),), subtitle: Text('The King in The North', textAlign: TextAlign.center, style: TextStyle( fontSize: 20),))
         ,SizedBox(height: 20, ),
         ListTile(title: Text('Bio', style: TextStyle( fontSize: 15, fontWeight: FontWeight.bold),),
-            subtitle: Text('My name is Magnus, but my mates call me Mag.\n''I got fed up of winning all the time'
-                ' so I thought I would try this app out since I have heard such great things about it.',
+            subtitle: Text(user.bio,
               style: TextStyle( fontSize: 15),)),
         ListTile(title: Text('Ability Level', style: TextStyle( fontSize: 15, fontWeight: FontWeight.bold),),
             subtitle: Text('Experienced (1600+)',
@@ -31,13 +44,15 @@ class UserPage extends StatelessWidget {
           style: ButtonStyle(
             foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
           ),
-          onPressed: () {Navigator.pushNamed(buildContext, '/edit_user_page');
+          onPressed: () {Navigator.pushNamed(buildContext, '/edit_user_page', arguments: user);
           EditProfilePage(); },
           child: Text('Edit Profile'),
         )
 
       ],
     )
+
     );
   }
+
 }
