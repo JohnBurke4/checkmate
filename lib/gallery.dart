15feda982c6e182d50f99723818b00c12c6282cd
Andrew Meehan <mandrew@tcd.ie>
2022-03-03 17:30:40 +0000

    allows user to access gallery only, updated dependencies and layout

--- lib/gallery.dart
+++ lib/gallery.dart
@@ -1,8 +1,22 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:photo_view/photo_view.dart';
 import 'package:photo_view/photo_view_gallery.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart' as path;
+
+List<String> urlImages = [
+  'https://imageio.forbes.com/specials-images/imageserve/61499e784c9631d3af55ed22/Magnus-Carlsen-Mastercard-promotional-headshot/960x0.jpg?fit=bounds&format=jpg&width=960',
+  'https://upload.wikimedia.org/wikipedia/commons/e/ec/FIDE_World_FR_Chess_Championship_2019_-_Magnus_Carlsen_%28cropped%29.jpg',
+  'https://upload.wikimedia.org/wikipedia/commons/a/aa/Carlsen_Magnus_%2830238051906%29.jpg',
+  'https://images.chesscomfiles.com/uploads/v1/news/895104.ba7ca489.668x375o.37bd1f5b4a08.jpeg',
+];
+
+List<File> _galleryImages = [];
 
 class Gallery extends StatefulWidget {
   const Gallery({Key? key}) : super(key: key);
@@ -12,13 +26,6 @@ class Gallery extends StatefulWidget {
 }
 
 class _GalleryState extends State<Gallery> {
-  final urlImages = [
-    'https://imageio.forbes.com/specials-images/imageserve/61499e784c9631d3af55ed22/Magnus-Carlsen-Mastercard-promotional-headshot/960x0.jpg?fit=bounds&format=jpg&width=960',
-    'https://upload.wikimedia.org/wikipedia/commons/e/ec/FIDE_World_FR_Chess_Championship_2019_-_Magnus_Carlsen_%28cropped%29.jpg',
-    'https://upload.wikimedia.org/wikipedia/commons/a/aa/Carlsen_Magnus_%2830238051906%29.jpg',
-    'https://images.chesscomfiles.com/uploads/v1/news/895104.ba7ca489.668x375o.37bd1f5b4a08.jpeg',
-  ];
-
   @override
   Widget build(BuildContext context) => Scaffold(
         body: Center(
@@ -27,6 +34,7 @@ class _GalleryState extends State<Gallery> {
               radius: 80,
               backgroundColor: Colors.white,
               backgroundImage: NetworkImage(urlImages.first),
+              //backgroundImage: (_galleryImages.first == null) ? Image.asset(assets\test\blank-profile-picture.png) : FileImage(_galleryImages.first),
             ),
             onTap: openGallery,
           ),
@@ -39,10 +47,6 @@ class _GalleryState extends State<Gallery> {
         ),
       ));
 }
-
-//class PickFromCameraRoll extends StatefulWidget{
-
-//}
 
 class GalleryWidget extends StatefulWidget {
   final PageController pageController;
@@ -78,4 +82,40 @@ class _GalleryWidgetState extends State<GalleryWidget> {
           },
         ),
       );
+}
+
+class ImageFromGallery extends StatefulWidget {
+  ImageFromGallery({Key? key}) : super(key: key);
+
+  @override
+  //_ImageFromGalleryState createState() => _ImageFromGalleryState();
+  _ImageFromGalleryState createState() => _ImageFromGalleryState();
+}
+
+class _ImageFromGalleryState extends State<ImageFromGallery> {
+  late File imageFile;
+  @override
+  Widget build(BuildContext context) => Container(
+        height: 45,
+        child: InkWell(
+          child: TextButton(
+            child: const Text('Edit Photos'),
+            style: TextButton.styleFrom(
+              textStyle: const TextStyle(fontSize: 20),
+            ),
+            onPressed: () => _openGallery(),
+          ),
+        ),
+      );
+
+  Future _openGallery() async {
+    final picture = await ImagePicker().pickImage(source: ImageSource.gallery);
+    this.setState(() {
+      if (picture != null) {
+        imageFile = File(picture!.path);
+      } else {
+        print('No image selected');
+      }
+    });
+  }
 }
--- lib/ui/views/edit_user_page.dart
+++ lib/ui/views/edit_user_page.dart
@@ -24,83 +24,77 @@ class _EditProfilePageState extends State<EditProfilePage> {
     }
 
     return Scaffold(
-        resizeToAvoidBottomInset: false,   //new line
+        resizeToAvoidBottomInset: false, //new line
         body: Column(
-      children: [
-        // ProfileWidget(
-        //   imagePath: user.imagePath,
-        //   isEdit: true,
-        //   onClicked: () async {},
-        // ),
-        const SizedBox(height: 30),
-        SizedBox(
-          height: 160,
-          child: Gallery(),
-        ),
-        SizedBox(height: 2),
-        TextButton(
-          style: TextButton.styleFrom(
-            textStyle: const TextStyle(fontSize: 20),
-          ),
-          onPressed: () {},
-          child: const Text('Edit Photos'),
-        ),
-        SizedBox(height: 2),
-        Container(
-          //alignment: Alignment.centerRight,
-          //padding: EdgeInsets.only(left:10),
-          width: 200,
-          child: TextFormField(
-            controller: nameController,
-            decoration: InputDecoration(
-                border: OutlineInputBorder(), labelText: 'Full Name'),
-          ),
-        ),
-        const SizedBox(height: 24),
-        Container(
-          padding: EdgeInsets.only(left: 10),
-          width: 600,
-          child: TextFormField(
-            controller: bioController,
-            maxLines: 4,
-            decoration:
-                InputDecoration(border: OutlineInputBorder(), labelText: 'Bio'),
-          ),
-        ),
-        const SizedBox(height: 24),
-        Container(
-          padding: EdgeInsets.only(left: 10),
-          width: 600,
-          child: TextFormField(
-            initialValue: 'Experienced (1600+)',
-            decoration: InputDecoration(
-                border: OutlineInputBorder(), labelText: 'Ability Level'),
-          ),
-        ),
-        // TextFieldWidget(
-        //   label: 'Email',
-        //   text: user.email,
-        //   onChanged: (email) {},
-        // ),
-        const SizedBox(height: 24),
-        ElevatedButton(
-          style: ButtonStyle(
-            foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
-          ),
-          onPressed: () {
-            user.name = nameController.text;
-            user.bio = bioController.text;
-            Navigator.pop(context, user);
-          },
-          child: Text('Confirm Changes'),
-        )
-        // TextFieldWidget(
-        //   label: 'About',
-        //   text: user.about,
-        //   maxLines: 5,
-        //   onChanged: (about) {},
-        // ),
-      ],
-    ));
+          children: [
+            // ProfileWidget(
+            //   imagePath: user.imagePath,
+            //   isEdit: true,
+            //   onClicked: () async {},
+            // ),
+            SizedBox(height: 30),
+            SizedBox(
+              height: 160,
+              child: Gallery(),
+            ),
+            SizedBox(height: 2),
+            ImageFromGallery(),
+            SizedBox(height: 2),
+            Container(
+              //alignment: Alignment.centerRight,
+              //padding: EdgeInsets.only(left:10),
+              width: 200,
+              child: TextFormField(
+                controller: nameController,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(), labelText: 'Full Name'),
+              ),
+            ),
+            const SizedBox(height: 24),
+            Container(
+              padding: EdgeInsets.only(left: 10),
+              width: 600,
+              child: TextFormField(
+                controller: bioController,
+                maxLines: 4,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(), labelText: 'Bio'),
+              ),
+            ),
+            const SizedBox(height: 24),
+            Container(
+              padding: EdgeInsets.only(left: 10),
+              width: 600,
+              child: TextFormField(
+                initialValue: 'Experienced (1600+)',
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(), labelText: 'Ability Level'),
+              ),
+            ),
+            // TextFieldWidget(
+            //   label: 'Email',
+            //   text: user.email,
+            //   onChanged: (email) {},
+            // ),
+            const SizedBox(height: 24),
+            ElevatedButton(
+              style: ButtonStyle(
+                foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+              ),
+              onPressed: () {
+                user.name = nameController.text;
+                user.bio = bioController.text;
+                Navigator.pop(context, user);
+              },
+              child: Text('Confirm Changes'),
+            )
+            // TextFieldWidget(
+            //   label: 'About',
+            //   text: user.about,
+            //   maxLines: 5,
+            //   onChanged: (about) {},
+            // ),
+          ],
+        ));
   }
 }
